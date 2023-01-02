1ae6e4b7274f7125ab95eeb99fa89bc1202361d0
Artur Faria Vieira <artur.f.vieira.94@gmail.com>
2023-01-02 20:29:01 -0300

    pastas, configs do deploy

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import router from './routes/indexRoutes.js';
+import cors from "cors";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Server running in port: ${port}`)) 
